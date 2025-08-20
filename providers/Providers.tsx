0191eac616b6eb99323f7a8c5a95c231880442f3
Kimpo <ak.kimpo@gmail.com>
2025-08-20 14:16:42 +0500

    fix: add MapProvider to Providers

--- providers/Providers.tsx
+++ providers/Providers.tsx
@@ -2,7 +2,12 @@
 import React from "react";
 import { Provider } from "react-redux";
 import { store } from "../lib/store";
+import { MapProvider } from "../providers/MapProvider";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-    return <Provider store={store}>{children}</Provider>;
+    return (
+        <Provider store={store}>
+            <MapProvider>{children}</MapProvider>
+        </Provider>
+    );
 }
